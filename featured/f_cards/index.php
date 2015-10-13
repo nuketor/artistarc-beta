0f1ce9bc66ade20f09a50596609f78d67c18fc53
TheeeX <karanjeet96@gmail.com>
2015-10-13 18:37:32 +0530

    post scroll function

---
+++ featured/f_cards/index.php
@@ -0,0 +1,38 @@
+
+<html>
+<title>Artistarc post Ininite scroll </title>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    
+    
+    <!-- Custom CSS -->
+    <link href="../css/style.css" rel="stylesheet">
+    <!--[panel]-->
+    <link href="../css/custom.bootstrap.css" rel="stylesheet">
+    <!--[padding, margin, grid]-->
+    <link href="../css/helper.css" rel="stylesheet" type="text/css">
+    
+    <!-- jQuery -->
+    <script src="../js/jquery.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+<!--     <link rel="stylesheet" type="text/css" href="style.css" /> -->
+</head>
+<body>
+<div id="mhead"><h2>Cards </h2></div>
+
+<img id='loading' src='img/loading.gif'>
+<div id="demoajax" cellspacing="0">
+</div>
+
+</body>
+<script type="text/javascript" src="script.js"></script>
+</html>
