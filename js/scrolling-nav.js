a737fcce3cf2b7c93b5596fbbdc981c7ef61ed87
TheeeX <karanjeet96@gmail.com>
2015-10-15 23:08:50 +0530

    changes for navbar

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,27 +1,47 @@
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(document).scrollTop() > 180) {
+    if ($(document).scrollTop() > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
+	$('#bs-example-navbar-collapse-1').removeClass("shrink");
+	$('#nav-main').removeClass('wat');
+	$('#faker').removeClass('expand');
 // 	$(".navbar-header").show();
+// 	$("#header2").hide();
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+	$('#bs-example-navbar-collapse-1').addClass("shrink");
+	$('#nav-main').addClass('wat');
+	$('#faker').addClass('expand');
 // 	$(".navbar-header").hide();
+// 	$("#header2").show();
     }
 });
 
 $(document).ready(function() {
-// 	$(".navbar-header").hide();
+ 	$('#nav-main').addClass('wat');
+	$('#faker').addClass('expand');
 });
 /*
         	$(window).scroll(function() {
-			if ($(document).scrollTop() > 150) {
-			$('.navbar').addClass('shrink');
+			if ($(document).scrollTop() > 50) {
+			$('#header2').addClass("shrink");
+			$('#bs-example-navbar-collapse-1').removeClass("shrink");
+			$('#header-logo').removeClass("shrink");
 			}
 			else {
-			$('.navbar').removeClass('shrink'); }
-			});
-*/
+			$('#header2').removeClass("shrink");
+			$('#bs-example-navbar-collapse-1').addClass("shrink");
+			$('#header-logo').addClass("shrink");
+			}
+			});*/
 
+
+$(document).ready(function(){
+    $('#commnt-box1').click(function(){
+//         $(this).parent().find('.collapse').fadeToggle(); 
+	alert('wtf');
+    });
+});
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
