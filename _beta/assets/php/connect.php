3255e4adac7a91b774b06fb63dabc0bd5668bd21
TheeeX <karanjeet96@gmail.com>
2015-10-16 23:27:48 +0530

    css js for beta v2.0

---
+++ _beta/assets/php/connect.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect ("localhost","root","ididit","artistarc_beta");
+if (mysqli_connect_errno()) {
+echo "failed to connect mysql: ". mysqli_connect_error();
+}
+/*else echo "sucess!";*/
+?>
